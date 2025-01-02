00f122ca924f0edb7af8afcc911f8a922a00b631
pão <marinalibanoguedes@gmail.com>
2025-01-02 18:46:58 +0000

    Update jogo forca.py

--- jogo forca.py
+++ jogo forca.py
@@ -1,29 +1,54 @@
 import random
-jogos=["hollow Knight", "little nightmares", "half life", "red dead redemption", "inside", "limbo", "roblox", "minecraft", "portal", "doom", "team fortress", "left for dead", "call of duty", "counter strike", "halo"]
-mangas=["my hero academia", "berserk", "vagabond", "assassination classroom", "oyasumi punpun", "tokyo ghoul", "hanako kun", "spy x family", "saiki k", "death note", "monster", "viland saga", "jujutsu kaisen", "sakamoto days"]
+jogos=["hollow Knight", "little nightmares", "half life", "red dead redemption", "inside", "limbo", "roblox", "minecraft", "portal", "doom", "team fortress", "left for dead", "call of duty", "counter strike", "halo", "celeste", "superliminal", "grand theft auto", "arkham asylum", "geometry dash", "super mario bros", "sonic & knuckles", "pac-man", "just dance", "guitar hero", "arkanoid", "pong", "space invaders", "pitfall", "donkey kong"]
+mangas=["my hero academia", "berserk", "vagabond", "assassination classroom", "oyasumi punpun", "tokyo ghoul", "hanako kun", "spy x family", "saiki k", "death note", "monster", "viland saga", "jujutsu kaisen", "sakamoto days", "dorohedoro", "naruto", "dragon ball", "pet shop of horrors", "hunter x hunter", "one piece", "black clover", "bleach", "haikyuu", "fullmetal alchemist", "one punch man", "pluto", "slam dunk", "akira", "jojo's bizzare adventure"]
+lista=[]
+listaerros=[]
+hp=5
 
-hp=10
 def jogo(palavra, letra):
     global hp
-    if letra in palavra: #complicar depois
-        print("correto")
-    else:
-        print("errado")
+    if letra not in palavra:
+        listaerros.append(letra)
         hp-=1
 
+def imprimir(palavra, lista):
+    resultado=""
+    for letra in palavra:
+        if letra in lista:
+            resultado=resultado+letra
+        elif letra == " ":
+            resultado=resultado+" "
+        else:
+            resultado=resultado+"_"
+    print(resultado)
+    return(resultado)
     
-jogador=int(input("escolha um tema: jogos(1) ou mangás(2):"))
-if jogador==1:
-    palavra=random.choice(jogos)
-elif jogador==2:
-    palavra=random.choice(mangas)
-else:
-    print("inválido!")
+def partida():
+    while True:
+        resultado=imprimir(palavra,lista)
+        if resultado == palavra:
+            print("você venceu!!!!")
+            break
+        print("letras erradas:", listaerros)
+        letra=input("escolha uma letra: ")
+        lista.append(letra)
+    
+        jogo(palavra, letra)
+        print("hp:", hp)
+        if hp == 0:
+            print("você perdeu... :(")
+            print(palavra)
+            break
 
 while True:
-    print(palavra) #tirar depois
-    print("número de caractéres: ", len(palavra))
-    letra=input("escolha uma letra: ")
-    
-    jogo(palavra, letra)
-    print("hp:", hp)
+    lista=[]
+    listaerros=[]
+    hp=5
+    jogador=int(input("escolha um tema: jogos(1) ou mangás(2):"))
+    if jogador==1:
+        palavra=random.choice(jogos)
+    elif jogador==2:
+        palavra=random.choice(mangas)
+    else:
+        print("inválido!")
+    partida()
