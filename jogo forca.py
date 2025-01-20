c120e9954b40878aa1011678077a53c71741c792
pão <marinalibanoguedes@gmail.com>
2025-01-20 18:01:24 +0000

    Update jogo forca.py

--- jogo forca.py
+++ jogo forca.py
@@ -60,9 +60,6 @@ homemforca=['''
  / \  |
       |
 =========''']
-abcnormal=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-abccapslock=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-pos=0
 
 def clear_console():
     if platform.system() == "Windows":
