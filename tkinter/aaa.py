c943e3104fb702c3995ad7efd43a4fd297b31964
pão <marinalibanoguedes@gmail.com>
2025-02-02 23:30:52 +0000

    Update aaa.py

--- tkinter/aaa.py
+++ tkinter/aaa.py
@@ -1,15 +1,28 @@
 import tkinter as tk
+num=0
 root=tk.Tk() #aqui que começamos a usar o tkinter
 
 root.title("amazing") #root é a janela, nome da janela
 
-label1=tk.Label(root, text="epic") #o root informa que o textino faz parte da root(janela), nome do textinho
+label1=tk.Label(root, text="epic", fg="white", bg="black") #o root informa que o textino faz parte da root(janela), nome do textinho
 label1.pack(pady=10) #tamanho da label
 
-label2=tk.Label(root, text="awesome")
+label2=tk.Label(root, text="awesome", fg="yellow", bg="red")
 label2.pack(pady=10)
 
-label3=tk.Label(root, text="cool")
+label3=tk.Label(root, text="cool", fg="pink", bg="green")
 label3.pack(pady=10)
+
+def mudar_texto():
+    button.config(text="parabéns!")
+
+button = tk.Button(root, text="me clique!", command=mudar_texto)
+button.pack(pady=10)
+
+def mudar_cor():
+    button2.config(text="wow!", bg="black", fg="red")
+
+button2 = tk.Button(root, text="mudar cor", command=mudar_cor)
+button2.pack(pady=10)
 
 root.mainloop() #fica aberta até eu fechar (loop)
