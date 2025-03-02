7c19763397329a969eb28543ad26de651ad6401a
pão <marinalibanoguedes@gmail.com>
2025-03-02 19:39:19 +0000

    Update jogo da velha.py

--- jogo da velha.py
+++ jogo da velha.py
@@ -6,7 +6,7 @@ venceu=False
 botoescheios=False
 
 root.title('Jogo da Velha')
-root.geometry('700x600+400+100')
+root.geometry('600x600')
 root.wm_resizable(width=False, height=False)
 
 def clicar0(botao):
@@ -62,41 +62,32 @@ def vencer_possibilidades():
     acabar_jogo(botao1, botao5, botao9)
     acabar_jogo(botao3, botao5, botao7)
     
-botao1 = tk.Button(root, text='', command=lambda: clicar0(botao1), font='Arial 14 bold')
-botao1.pack(pady=10)
-botao1.place(x=40, y=20, width=200, height=160)
+botao1 = tk.Button(root, text='', command=lambda: clicar0(botao1), font='Arial 150')
+botao1.place(x=0, y=0, width=200, height=200)
 
-botao2 = tk.Button(root, text='', command=lambda: clicar0(botao2), font='Arial 14 bold')
-botao2.pack(pady=10)
-botao2.place(x=260, y=20, width=200, height=160)
+botao2 = tk.Button(root, text='', command=lambda: clicar0(botao2), font='Arial 150')
+botao2.place(x=200, y=0, width=200, height=200)
 
-botao4 = tk.Button(root, text='', command=lambda: clicar0(botao4), font='Arial 14 bold')
-botao4.pack(pady=10)
-botao4.place(x=40, y=200, width=200, height=160)
+botao3 = tk.Button(root, text='', command=lambda: clicar0(botao3), font='Arial 150')
+botao3.place(x=400, y=0, width=200, height=200)
 
-botao5 = tk.Button(root, text='', command=lambda: clicar0(botao5), font='Arial 14 bold')
-botao5.pack(pady=10)
-botao5.place(x=260, y=200, width=200, height=160)
+botao4 = tk.Button(root, text='', command=lambda: clicar0(botao4), font='Arial 150')
+botao4.place(x=0, y=200, width=200, height=200)
 
-botao6 = tk.Button(root, text='', command=lambda: clicar0(botao6), font='Arial 14 bold')
-botao6.pack(pady=10)
-botao6.place(x=480, y=200, width=200, height=160)
+botao5 = tk.Button(root, text='', command=lambda: clicar0(botao5), font='Arial 150')
+botao5.place(x=200, y=200, width=200, height=200)
 
-botao3 = tk.Button(root, text='', command=lambda: clicar0(botao3), font='Arial 14 bold')
-botao3.pack(pady=10)
-botao3.place(x=480, y=20, width=200, height=160)
+botao6 = tk.Button(root, text='', command=lambda: clicar0(botao6), font='Arial 150')
+botao6.place(x=400, y=200, width=200, height=200)
 
-botao9 = tk.Button(root, text='', command=lambda: clicar0(botao9), font='Arial 14 bold')
-botao9.pack(pady=10)
-botao9.place(x=480, y=400, width=200, height=160)
+botao7 = tk.Button(root, text='', command=lambda: clicar0(botao7), font='Arial 150')
+botao7.place(x=0, y=400, width=200, height=200)
 
-botao8 = tk.Button(root, text='', command=lambda: clicar0(botao8), font='Arial 14 bold')
-botao8.pack(pady=10)
-botao8.place(x=260, y=400, width=200, height=160)
+botao8 = tk.Button(root, text='', command=lambda: clicar0(botao8), font='Arial 150')
+botao8.place(x=200, y=400, width=200, height=200)
 
-botao7 = tk.Button(root, text='', command=lambda: clicar0(botao7), font='Arial 14 bold')
-botao7.pack(pady=10)
-botao7.place(x=40, y=400, width=200, height=160)
+botao9 = tk.Button(root, text='', command=lambda: clicar0(botao9), font='Arial 150')
+botao9.place(x=400, y=400, width=200, height=200)
 
 botoes=[botao1, botao2, botao3, botao4, botao5, botao6, botao7, botao8, botao9]
 
