695fa15ef86858f459a73c1ef391e109a78fef61
pão <marinalibanoguedes@gmail.com>
2025-03-17 18:26:13 +0000

    Update fun_ficheiro.py

--- ficheiros/fun_ficheiro.py
+++ ficheiros/fun_ficheiro.py
@@ -1,6 +1,6 @@
 def criar_ficheiro():
     global ficheiro
-    ficheiro=input("nome do ficheiro: ")
+    ficheiro=input("nome do ficheiro: ") + ".txt"
     texto=input("texto: ")
 
     try:
@@ -11,13 +11,13 @@ def criar_ficheiro():
     	print("erro ao criar ficheiro {e}")
 
 def adicionar_texto():
-    global ficheiro
-    add=input("adicionar ao ficheiro: ")
-    with open (ficheiro, 'a') as ficheiro4:
-        ficheiro4.write('\n'+add)
-        print("frase adicionada com succeso")
+    try:
+        add=input("adicionar ao ficheiro: ")
+        with open (ficheiro, 'a') as ficheiro4:
+            ficheiro4.write('\n'+add)
+            print("frase adicionada com succeso")
+    except Exception as e:
+    	print("o fichero "+ficheiro+"não existe")
 
 criar_ficheiro()
 adicionar_texto()
-
-
