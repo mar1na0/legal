d6acc1be4e3216079e9f380640dafb3e58617194
pão <marinalibanoguedes@gmail.com>
2025-03-31 18:31:35 +0100

    Update imc.py

--- tkinter/imc.py
+++ tkinter/imc.py
@@ -2,8 +2,8 @@ from tkinter import *
 import math
 
 tela1=Tk()
-tela1.title("awesome")
-tela1.geometry('380x500+500+100')
+tela1.title("calculadora de imc")
+tela1.geometry('350x200')
 tela1.wm_resizable(width=False, height=False)
 
 laltura=Label(tela1, text="altura", font='Time 10', anchor='w')
@@ -11,7 +11,7 @@ laltura.place(x=10, y=30, width=60, height=20)
 input_altura=Entry(tela1, font='Time 10')
 input_altura.place(x=50, y=30, width=250, height=20)
 
-lpeso=Label(tela1, text="peso", font='Time 10', anchor='w') #anchor coloca pro lado
+lpeso=Label(tela1, text="peso", font='Time 10', anchor='w')
 lpeso.place(x=10, y=70, width=60, height=20)
 input_peso=Entry(tela1, font='Time 10')
 input_peso.place(x=50, y=70, width=250, height=20)
@@ -23,14 +23,15 @@ limc=Label(tela1, text="IMC=", font='Time 10', anchor='w')
 limc.place(x=50, y=160, width=60, height=20)
 
 limc0=Label(tela1, text="???", font='Time 10', anchor='w')
-limc0.place(x=80, y=160, width=60, height=20)
+limc0.place(x=80, y=160, width=50, height=20)
 
 def calcular():
     altura=int(input_altura.get())/100
     peso=int(input_peso.get())
-
     imc= peso/altura**2
+    limc0.config(text=imc, fg='green')
 
-    limc0.config(text=imc)
+    if imc<18 or imc>25:
+        limc0.config(fg='red')
 
 tela1.mainloop()
